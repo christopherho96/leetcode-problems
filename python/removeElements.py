cc7a355593f54f073fbc37e4fcd753b3a2ce2e61
Christopher Ho <christopherho@Christophers-MacBook-Pro.local>
2019-07-23 22:27:55 -0400

    easy remove all instances of val, then return remaining length

---
+++ python/removeElements.py
@@ -0,0 +1,12 @@
+https://leetcode.com/problems/remove-element/submissions/
+class Solution(object):
+    def removeElement(self, nums, val):
+        left = 0
+        right = len(nums)-1
+        while left<=right:
+            if nums[left] == val:
+                nums[left] = nums[right]
+                right -= 1
+            else:
+                left += 1
+        return right + 1        
